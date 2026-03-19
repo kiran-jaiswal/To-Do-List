2193c81e17ff13255b28b3c52dc31c5978c16048
Kiran Jaiswal <kiranjaiswalkj2002@gmail.com>
2026-03-19 15:38:53 +0530

    initial commit

---
+++ src/main/java/com/example/To_Do/Task/Manager/ToDoTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.To_Do.Task.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoTaskManagerApplication.class, args);
+	}
+
+}
